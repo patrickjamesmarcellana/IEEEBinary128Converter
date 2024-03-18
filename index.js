90511676a059bf814184fa5a017ca8ba88dfa540
Harvey Chua <harvs89.2@gmail.com>
2024-03-18 16:26:32 +0800

    Load converter.js on client side

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import browserify from "browserify"
 import express from "express"
+import fs from "fs"
 import { router } from "./src/routes/routes.js"
 import "dotenv/config"
+import watchify from "watchify"
 
 const app = express()
 
@@ -18,3 +21,14 @@ app.use((err, req, res, next) => {
 app.listen(process.env.PORT, () => {
     console.log(`IEEE-754 Binary-128 Floating Point Converter App now listening to port ${process.env.PORT}...`)
 })
+
+const b = browserify(['converter.js'], {
+    plugin: [watchify]
+})
+const bunlde = () => {
+    b.bundle()
+        .on('error', console.error)
+        .pipe(fs.createWriteStream('public/js/bundle.js'))
+}
+b.on('update', bunlde)
+bunlde()
