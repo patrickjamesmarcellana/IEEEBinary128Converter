f98a94a807dfee254807d083b791f7b175fa14d6
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2024-03-18 21:11:07 +0800

    Append zeros to exponent bits if less than 15 in length
    Also format converter.js with Prettier

--- converter.js
+++ converter.js
@@ -1,13 +1,12 @@
-const Decimal = require('decimal.js');
+const Decimal = require("decimal.js");
 
 var shift_forward = -1;
 var binary = [];
-;
-var decimal_val = '15000000000'
-var actual_val = ''
-var binary_val = '';
-var error_val = '';
-var hex_val = '';
+var decimal_val = "505.5";
+var actual_val = "";
+var binary_val = "";
+var error_val = "";
+var hex_val = "";
 var sign_bit = [];
 var exponent_bits = [];
 var mantissa_bits = [];
@@ -19,7 +18,7 @@ function updateFromNewDecimalString() {
     Decimal.set({ precision: 150 });
     var x = new Decimal(decimal_val);
     Decimal.rounding = Decimal.ROUND_DOWN;
-    if (x.greaterThanOrEqualTo(0)){
+    if (x.greaterThanOrEqualTo(0)) {
         sign_bit = [0];
     } else {
         sign_bit = [1];
@@ -29,19 +28,19 @@ function updateFromNewDecimalString() {
     var number_bits_total = number_binary.length;
 
     var decimal_binary = getDecimalBinary(x, 113 - number_bits_total);
-    if (number_binary.length == 0){
+    if (number_binary.length == 0) {
         decimal_binary = popUntilFirstOne(decimal_binary);
     }
-    if (decimal_binary.length > 112 - number_bits_total){
-        if (decimal_binary[113 - number_bits_total] == 1){
-            decimal_binary = roundUpBinary(decimal_binary)
+    if (decimal_binary.length > 112 - number_bits_total) {
+        if (decimal_binary[113 - number_bits_total] == 1) {
+            decimal_binary = roundUpBinary(decimal_binary);
             decimal_binary.pop();
         } else {
             decimal_binary.pop();
-        };
+        }
     }
-    console.log(number_binary.join(''));
-    console.log(decimal_binary.join(''));
+    console.log(number_binary.join(""));
+    console.log(decimal_binary.join(""));
 
     var exponent = 0;
     if (number_bits_total > 0) {
@@ -52,51 +51,65 @@ function updateFromNewDecimalString() {
 
     var exponent_binary = new Decimal(16383 + exponent);
     exponent_binary = getNumberBinary(exponent_binary);
-    exponent_bits = exponent_binary;
-    console.log(exponent_bits.join(''));
+    exponent_bits = Array(15 - exponent_binary.length)
+        .fill(0)
+        .concat(exponent_binary);
+    console.assert(
+        exponent_bits.length === 15,
+        "exponent bits length: expected 15, got",
+        exponent_bits.length
+    );
+    console.log(exponent_bits.join(""));
 
-    var mantissa = []
+    var mantissa = [];
     mantissa = number_binary.slice(1);
     mantissa = mantissa.concat(decimal_binary);
     mantissa_bits = addZerosToArray(mantissa, 112);
-    console.log(addSpaces(mantissa_bits.join('')));
+    console.assert(
+        mantissa_bits.length === 112,
+        "mantissa bits length: expected 112, got",
+        mantissa_bits.length
+    );
+    console.log(addSpaces(mantissa_bits.join("")));
 
-    binary_val = sign_bit.join('') + exponent_bits.join('') + mantissa_bits.join('')
+    binary_val =
+        sign_bit.join("") + exponent_bits.join("") + mantissa_bits.join("");
     console.log(binary_val);
 
     var hex_string = convertBinaryStringToHex();
-    hex_val = hex_string.join('');
+    hex_val = hex_string.join("");
     console.log(hex_val);
 
     getDecimalFromBinary();
     getError();
 }
 
-function updateFromNewBinaryString(){
+function updateFromNewBinaryString() {
     // Use when binary_val has changed.
     // All other values will change accordingly.
     Decimal.set({ precision: 150 });
 
     var binary_string = binary_val;
-    sign_bit = binary_string.slice(0, 1).split('');
-    exponent = binary_string.slice(1, 12).split('');
-    mantissa = binary_string.slice(12).split('');
+    sign_bit = binary_string.slice(0, 1).split("");
+    exponent = binary_string.slice(1, 12).split("");
+    mantissa = binary_string.slice(12).split("");
 
     getDecimalFromBinary();
-    decimal_val = actual_val
-    hex_val = convertBinaryStringToHex.join('');
+    decimal_val = actual_val;
+    hex_val = convertBinaryStringToHex.join("");
     getError();
 }
 
-function updateFromNewBinaryToggle(){
+function updateFromNewBinaryToggle() {
     // Use when a binary bit is changed.
     // All other calues will change accordingly.
     Decimal.set({ precision: 150 });
 
-    binary_val = sign_bit.join('') + exponent_bits.join('') + mantissa_bits.join('');
+    binary_val =
+        sign_bit.join("") + exponent_bits.join("") + mantissa_bits.join("");
     getDecimalFromBinary();
-    decimal_val = actual_val
-    hex_val = convertBinaryStringToHex.join('');
+    decimal_val = actual_val;
+    hex_val = convertBinaryStringToHex.join("");
     getError();
 }
 
@@ -104,32 +117,37 @@ function getDecimalFromBinary() {
     // Takes the current binary bits and gets the actual decimal value.
     // Will modify actual_val.
 
-    var exponent = binaryToNumber(exponent_bits.join(''));
+    var exponent = binaryToNumber(exponent_bits.join(""));
     exponent = exponent.sub(16383);
-    var mantissa = mantissa_bits.join('');
+    var mantissa = mantissa_bits.join("");
     if (exponent < 0) {
-        var fractional_string = addZerosAndOneToBeginning(mantissa, exponent.negated());
+        var fractional_string = addZerosAndOneToBeginning(
+            mantissa,
+            exponent.negated()
+        );
         var fractional = binaryToFractional(fractional_string);
         actual_val = fractional.toFixed();
-    } else if (exponent < mantissa_bits.length){
-        var number_string = '1' + mantissa.slice(0, exponent);
+    } else if (exponent < mantissa_bits.length) {
+        var number_string = "1" + mantissa.slice(0, exponent);
         var fractional_string = mantissa.slice(exponent);
         var number_string_val = binaryToNumber(number_string).toPrecision();
-        var fractional_string_val = binaryToFractional(fractional_string).toDP(30).toFixed();
+        var fractional_string_val = binaryToFractional(fractional_string)
+            .toDP(30)
+            .toFixed();
         actual_val = number_string_val + fractional_string_val.slice(1);
-    }
-    else {
-        var number_string = '1' + addZerosToEnd(mantissa, exponent - mantissa_bits.length);
+    } else {
+        var number_string =
+            "1" + addZerosToEnd(mantissa, exponent - mantissa_bits.length);
         var number_string_val = binaryToNumber(number_string).toPrecision();
         actual_val = number_string_val;
     }
 
-    if (sign_bit[0] == 1){
-        actual_val = '-' + actual_val
+    if (sign_bit[0] == 1) {
+        actual_val = "-" + actual_val;
     }
 }
 
-function getError(){
+function getError() {
     // Gets the error between actual_val and decimal_val and stores it in error_val
 
     var decimal = new Decimal(decimal_val);
@@ -146,8 +164,7 @@ function binaryToFractional(string) {
     var power = new Decimal(2);
 
     for (let i = 0; i < string.length; i++) {
-
-        if (string[i] === '1') {
+        if (string[i] === "1") {
             decimal = decimal.add(one.div(power));
         }
         power = power.mul(2);
@@ -165,7 +182,7 @@ function binaryToNumber(string) {
     // Iterate over the binary string from right to left
     for (let i = string.length - 1; i >= 0; i--) {
         // If the current bit is '1', add the corresponding power of 2 to the decimal value
-        if (string[i] === '1') {
+        if (string[i] === "1") {
             decimal = decimal.add(power);
         }
         // Update the power of 2 for the next bit
@@ -178,24 +195,24 @@ function binaryToNumber(string) {
 function roundUpBinary(arr) {
     // Rounds binary
     var i = arr.length - 1;
-    while (arr[i] != 0){
+    while (arr[i] != 0) {
         arr[i] = 0;
-        i--
+        i--;
     }
     arr[i] = 1;
     return arr;
 }
 
-function convertBinaryStringToHex(){
+function convertBinaryStringToHex() {
     // Converts a given binary string into hex. Assumes it is a string divisible by 4.
-    var hex_string = []
+    var hex_string = [];
     var split4 = binary_val.match(/.{1,4}/g);
 
-    split4.forEach(split => {
+    split4.forEach((split) => {
         hex_string.push(parseInt(split, 2).toString(16).toUpperCase());
     });
 
-    return hex_string
+    return hex_string;
 }
 
 function addZerosToArray(arr, desiredLength) {
@@ -206,36 +223,36 @@ function addZerosToArray(arr, desiredLength) {
     return arr;
 }
 
-function addZerosAndOneToBeginning(str, num){
+function addZerosAndOneToBeginning(str, num) {
     // Adds a set of zeros and one to a string
     // For numbers where exponent moves the fixed point back past the original value
-    var zeros = '0'.repeat(num - 1);
-    return zeros + '1' + str;
+    var zeros = "0".repeat(num - 1);
+    return zeros + "1" + str;
 }
 
-function addZerosToEnd(str, num){
+function addZerosToEnd(str, num) {
     // Adds a set of zeros to the end of a string
-    var zeros = '0'.repeat(num - 1);
+    var zeros = "0".repeat(num - 1);
     return str + zeros;
 }
 
 function addSpaces(str) {
     // Splits number into sections of 4 for visual purposes.
-    return str.replace(/(.{4})/g, '$1 ');
+    return str.replace(/(.{4})/g, "$1 ");
 }
 
-function popUntilFirstOne(arr){
-    while(arr[0] == 0){
+function popUntilFirstOne(arr) {
+    while (arr[0] == 0) {
         arr = arr.slice(1);
     }
     return arr;
 }
 
-function getNumberBinary(number){
+function getNumberBinary(number) {
     // Gets the binary bits of a number
     var number_portion = number.round();
     var binary_bits = [];
-    while(!number_portion.equals(0)){
+    while (!number_portion.equals(0)) {
         var cur_binary = number_portion.modulo(2);
         binary_bits.push(cur_binary.toNumber());
         number_portion = number_portion.div(2).round();
@@ -243,20 +260,20 @@ function getNumberBinary(number){
     return binary_bits.reverse();
 }
 
-function getDecimalBinary(number, limit){
+function getDecimalBinary(number, limit) {
     // Gets the binary bits of a fractional number
     var decimal_portion = number.minus(number.round());
     var binary_bits = [];
     var current_bit_total = 0;
     var no_number = false;
     var zero_trigger = true;
-    if (limit == 121){
+    if (limit == 121) {
         no_number = true;
     }
 
-    while(current_bit_total <= limit && !decimal_portion.equals(0)){
+    while (current_bit_total <= limit && !decimal_portion.equals(0)) {
         decimal_portion = decimal_portion.mul(2);
-        if (decimal_portion.greaterThanOrEqualTo(1)){
+        if (decimal_portion.greaterThanOrEqualTo(1)) {
             decimal_portion = decimal_portion.minus(1);
             binary_bits.push(1);
             zero_trigger = false;
@@ -264,7 +281,7 @@ function getDecimalBinary(number, limit){
             binary_bits.push(0);
         }
         current_bit_total = current_bit_total + 1;
-        if (zero_trigger){
+        if (zero_trigger) {
             current_bit_total = current_bit_total - 1;
             shift_forward = shift_forward - 1;
         }
@@ -276,33 +293,33 @@ function loadBinaryString(binary_string, exponent) {
     let sign = false;
     let dot_idx = null;
     let first_one_idx = null;
-    const fractional = []
-    for(let i = 0; i < binary_string.length; i++) {
+    const fractional = [];
+    for (let i = 0; i < binary_string.length; i++) {
         const char = binary_string[i];
 
         // push to fractional if implicit 1 already found
-        if(first_one_idx != null && (char == '0' || char == '1')) {
+        if (first_one_idx != null && (char == "0" || char == "1")) {
             fractional.push(char);
         }
 
         // parse input
-        switch(char) {
-            case '0':
+        switch (char) {
+            case "0":
                 break;
-            case '1':
+            case "1":
                 // check if this is implicit 1
-                if(first_one_idx == null) {
+                if (first_one_idx == null) {
                     first_one_idx = i;
                 }
-                break
-            case '.':
-                if(dot_idx == null) {
+                break;
+            case ".":
+                if (dot_idx == null) {
                     dot_idx = i;
                 } else {
                     // error
                 }
-            case '-':
-                if(i == 0) {
+            case "-":
+                if (i == 0) {
                     sign = true;
                 } else {
                     // error
@@ -311,20 +328,24 @@ function loadBinaryString(binary_string, exponent) {
     }
 
     // determine exponent
-    exponent = parseInt(exponent)
-    if(dot_idx != null) { //
-        if(first_one_idx < dot_idx) { // has dot
+    exponent = parseInt(exponent);
+    if (dot_idx != null) {
+        //
+        if (first_one_idx < dot_idx) {
+            // has dot
             // move towards 1, but not so they would end up switching positions
             exponent += dot_idx - first_one_idx - 1;
-        } else { // 000.xx1xxxx
+        } else {
+            // 000.xx1xxxx
             // move towards 1, and make them switch positions
             exponent += dot_idx - first_one_idx;
         }
-    } else { // no dot
+    } else {
+        // no dot
         exponent += fractional.length;
     }
 
-    console.log(binary_string, sign, fractional, exponent)
+    console.log(binary_string, sign, fractional, exponent);
 }
 updateFromNewDecimalString();
 
