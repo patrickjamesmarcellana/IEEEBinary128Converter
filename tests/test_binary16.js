84a03305d75a6696f88f280ab8db44bfeb6a4baf
Harvey Chua <harvs89.2@gmail.com>
2024-03-19 19:25:19 +0800

    Add tests with inexact numbers

--- tests/test_binary16.js
+++ tests/test_binary16.js
@@ -1,16 +1,23 @@
 import {loadBinaryString, loadDecimalString, toBinary16} from "../converter.js"
 import binary16_data from  "./binary16_values.js"
+import binary16_data_floatmode_unique from  "./binary16_values_floatmode_unique.js"
 
 
 toBinary16();
-for(const [decimal, expected] of binary16_data) {
-    console.log('Testing', decimal)
-    const {hex, bin} = loadDecimalString(decimal, '0')
-    
-    if(hex === expected.toUpperCase()) {
-        console.log("pass")
-    } else {
-        console.log("fail", decimal, "actual:", hex, "expected:", expected.toUpperCase())
-        process.exit(1)
+
+const test = (data) => {
+    for(const [decimal, expected] of data) {
+        console.log('Testing', decimal)
+        const {hex, bin} = loadDecimalString(decimal, '0')
+        
+        if(hex === expected.toUpperCase()) {
+            console.log("pass")
+        } else {
+            console.log("fail", decimal, "actual:", hex, "expected:", expected.toUpperCase())
+            process.exit(1)
+        }
     }
 }
+
+test(binary16_data)
+test(binary16_data_floatmode_unique)
