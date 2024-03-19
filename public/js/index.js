f1d83559002fd234a2ba77f58ca7e5a8153bbbfc
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2024-03-19 11:41:46 +0800

    Store input values when switching between binary and decimal
    Also reorganize code and format with Prettier

--- public/js/index.js
+++ public/js/index.js
@@ -1,63 +1,84 @@
-$("#decimal-input").click((e) => {
-    $("#binary-input").removeClass("opacity-100 chosen")
-    $("#binary-input").addClass("opacity-25 hover:opacity-100")
-    $("#decimal-input").addClass("opacity-100 chosen")
-    $("decimal-input").removeClass("hover:opacity-100")
+// records values so they are not lost when switching base/radix
+var binaryNumberString = "";
+var binaryExponentString = "";
+var decimalNumberString = "";
+var decimalExponentString = "";
 
-    $("#number-input-label").text("Decimal Value")
-    $("#exponent-input-label").text("Base-10 Exponent")
-})
+// 2 stands for binary mantissa input,
+// 10 stands for decimal mantissa input
+var MODE = 2;
 
-$("#binary-input").click((e) => {
-    $("#decimal-input").removeClass("opacity-100 chosen")
-    $("#decimal-input").addClass("opacity-25 hover:opacity-100")
-    $("#binary-input").addClass("opacity-100 chosen")
-    $("binary-input").removeClass("hover:opacity-100")
-
-    $("#number-input-label").text("Binary Mantissa")
-    $("#exponent-input-label").text("Base-2 Exponent")
-})
-
-$("#clear-btn").click((e) => {
-    $("#number-value").val("") 
-    $("#exponent-value").val("")
-    console.log(MODE)
-    console.log(NUMBER)
-    console.log(EXPONENT)
-})
-
-var MODE = 2; var MODE = 2; // 2 stands for binary mantissa input, 
-                            // 10 stands for decimal mantissa input
-                            // this value will be changed by clicking the submit button
-
-var NUMBER = 0;             // this value will be changed by clicking the submit button
-
-var EXPONENT = 0;           // this value will be changed by clicking the submit button
+// stores the values of the number and exponent text fields
+var NUMBER = 0;
+var EXPONENT = 0;
 
 $("#submit-btn").click((e) => {
-    const binary_input_button = document.getElementById("binary-input")
-    if(binary_input_button.classList.contains('chosen')) {
+    if ($("#binary-input").hasClass("active")) {
         MODE = 2;
     } else {
         MODE = 10;
     }
 
-    NUMBER = $("#number-value").val()
-    EXPONENT = $("#exponent-value").val()
+    NUMBER = $("#number-value").val();
+    EXPONENT = $("#exponent-value").val();
 
     let result;
-    switch(MODE) {
+    switch (MODE) {
         case 2:
-            result = window.loadBinaryString(NUMBER, EXPONENT)
-            break
+            result = window.loadBinaryString(NUMBER, EXPONENT);
+            break;
         case 10:
-            result = window.loadDecimalString(NUMBER, EXPONENT)
-            break
+            result = window.loadDecimalString(NUMBER, EXPONENT);
+            break;
     }
-    $("#output-bin").text(result.bin)
-    $("#output-hex").text(result.hex)
-})
+    $("#output-bin").text(result.bin);
+    $("#output-hex").text(result.hex);
+});
 
+$("#decimal-input").click((e) => {
+    if ($("#decimal-input").hasClass("active")) {
+        return;
+    }
 
+    $("#binary-input").removeClass("opacity-100 active");
+    $("#binary-input").addClass("opacity-25 hover:opacity-100");
+    $("#decimal-input").addClass("opacity-100 active");
+    $("#decimal-input").removeClass("hover:opacity-100");
 
+    $("#number-input-label").text("Decimal Value");
+    $("#exponent-input-label").text("Base-10 Exponent");
 
+    // store values so they are not lost when switching radix
+    binaryNumberString = $("#number-value").val();
+    binaryExponentString = $("#exponent-value").val();
+    $("#number-value").val(decimalNumberString);
+    $("#exponent-value").val(decimalExponentString);
+});
+
+$("#binary-input").click((e) => {
+    if ($("#binary-input").hasClass("active")) {
+        return;
+    }
+
+    $("#decimal-input").removeClass("opacity-100 active");
+    $("#decimal-input").addClass("opacity-25 hover:opacity-100");
+    $("#binary-input").addClass("opacity-100 active");
+    $("#binary-input").removeClass("hover:opacity-100");
+
+    $("#number-input-label").text("Binary Mantissa");
+    $("#exponent-input-label").text("Base-2 Exponent");
+
+    // store values so they are not lost when switching radix
+    decimalNumberString = $("#number-value").val();
+    decimalExponentString = $("#exponent-value").val();
+    $("#number-value").val(binaryNumberString);
+    $("#exponent-value").val(binaryExponentString);
+});
+
+$("#clear-btn").click((e) => {
+    $("#number-value").val("");
+    $("#exponent-value").val("");
+    console.log(MODE);
+    console.log(NUMBER);
+    console.log(EXPONENT);
+});
